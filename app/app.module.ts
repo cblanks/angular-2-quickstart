b2f15ec19248e08dd080d3e3c7bde029c5fdc5ae
Chris Blanks <Chris Blanks>
2016-10-11 01:01:45 +0100

    update web service.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,50 +1,55 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { RouterModule }  from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroService } from './hero.service';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
 
 @NgModule({
-	imports: [ 
-		BrowserModule, 
-		FormsModule,
-		RouterModule.forRoot([
-			{
-				path: '',
-				redirectTo: '/dashboard',
-				pathMatch: 'full'
-			},
-			{
-				path: 'heroes',
-				component: HeroesComponent
-			},
-			{
-				path: 'dashboard',
-				component: DashboardComponent
-			},
-			{
-				path: 'detail/:id',
-				component: HeroDetailComponent
-			}
-		])
-	],
-	declarations: [ 
-		AppComponent, 
-		DashboardComponent,
-		HeroDetailComponent,
-		HeroesComponent
-	],
-	providers: [
-		HeroService
-	],
-	bootstrap: [ 
-		AppComponent 
-	]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+  ],
+  providers: [
+    HeroService,
+  ],
+  bootstrap: [ AppComponent ]
 })
-
-export class AppModule {}
+export class AppModule {
+}
